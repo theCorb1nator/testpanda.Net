22540ce9484ed7ac759bc1a27a0c92f6e9c56d96
corbin taylor <corbin@dmsnavigator.co.uk>
2019-01-12 00:57:20 +0000

    added missing folder

---
+++ src/TestPanda.Core/Models/TestCase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestPanda.Core
+{
+    public class TestCase : ITestCase
+    {
+        public string Description { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public TestState State { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime LastUpdated { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Comments { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int Id => throw new NotImplementedException();
+    }
+}
