9fe03bbb80b327f6432978e0fa17673de9cd255d
corbin taylor <corbin@dmsnavigator.co.uk>
2019-01-13 16:31:02 +0000

    testing the damn thing syncs

--- src/TestPanda.Api/Models/UpdateTestCaseStateDto.cs
+++ src/TestPanda.Api/Models/UpdateTestCaseStateDto.cs
@@ -5,6 +5,7 @@
         public int TestCaseId { get; set; }
         public string Title { get; set; }
         public string State { get; internal set; }
+        //
         //TODO Add who updated the test state 
     }
 }
